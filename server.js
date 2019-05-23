aa6e833735ce15763d28fe00efc2f0db45972880
Tevvin <Tevvin7395@icloud.com>
2019-05-23 01:44:44 -0400

    MVP complete need to fix bugs

--- server.js
+++ server.js
@@ -28,14 +28,38 @@ server.post('/', (req, res) => {
     let createAccount = {
         name: req.body.name,
         budget: req.body.budget
-    };
+    }
 
     db.add(createAccount)
-        .then(accounts => {
-            res.status(200).json(accounts);
+        .then(account => {
+            res.status(200).json(account);
         })
         .catch(error => {
             res.status(404).json({ error: "that specified id dones not exists." });
+        })
+});
+
+server.put('/:id', (req, res) => {
+    const updateAccount = req.body;
+    console.log(updateAccount);
+    db.update(updateAccount)
+        .then(updated => {
+            res.status(200).json(updated)
+        })
+        .catch(error => {
+            res.status(404).json({ error: "Unable to update this specified account." });
+        })
+});
+
+server.delete('/:id', (req, res) => {
+    let deleteAccount = req.params.id;
+
+    db.remove(deleteAccount)
+        .then(deleted => {
+            res.status(200).json(deleted);
+        })
+        .catch(error => {
+            res.status(404).json({ error: "Unable to delete the specified account." });
         })
 });
 
